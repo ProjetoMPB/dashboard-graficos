e9dd541b7252c416963192d528e841aaa162b060
Felipe Martins <felipe.martins4@hotmail.com>
2025-01-21 17:01:57 -0300

    Refatora c-letras

--- carrega_dados.py
+++ carrega_dados.py
@@ -1,89 +1,30 @@
 import pandas as pd
 from collections import Counter
 
+
 def carrega_c_letras():
-    # abrir a lista acumulada de c-palavras
-    c_palavras=[]
-    with open('c_palavras_corpora.txt', 'r') as file:
-        for linha in file:
-            c_palavras.append(linha)
+    df = pd.read_csv(r'resultados\2M2b.csv', index_col=0)
+    df *= 100
+    corpora = df.columns.values.tolist()
 
-    # normalização
-    x=c_palavras
-    c_palavras1=[]
+    saida = {
+        'df': df,
+        'x': pd.DataFrame({corpus: ['u', 'P', 'p', 'A', 'a', 'S', 's'] for corpus in corpora}),
+        'hover_data': {
+            corpus: {
+                'u': 'Movimento: Uníssono',
+                 'P': 'Movimento: Passo ascendente',
+                 'p': 'Movimento: Passo descendente',
+                 'A': 'Movimento: Arpejo ascendente',
+                 'a': 'Movimento: Arpejo descendente',
+                 'S': 'Movimento: Salto ascendente',
+                 's': 'Movimento: salto descendente'
+            } for corpus in corpora
+        },
+        'graficos_por_linha': 4,
+        'titulo_do_grafico': 'Porcentagem de ocorrência das c-letras em cada corpus selecionado'
+    }
 
-    # Remover as aspas externas de cada elemento
-    x = [item.strip("'") for item in x]
-
-    # Converter cada elemento em uma lista usando eval()
-    x = [eval(item) for item in x]
-
-    # Imprimir nova lista
-    for item in x:
-        c_palavras1.extend(item)
-
-    c_palavras1_dict = {c_palavras1[k]:c_palavras1[k + 1] for k in range(0, len(c_palavras1) - 1, 2)}
-
-    c_letras_pct_dict = {}
-
-    for corpus in c_palavras1_dict.keys():
-        # Iniciar a contabilização de c-letras
-        # vetor c-letras
-        v_c_letras=7*[0]
-
-        prv=[]
-        for k in c_palavras1_dict[corpus]:
-            a=k
-            for j in a:
-                if j=='u':
-                    v_c_letras[0]=v_c_letras[0]+1
-                elif j=='P':
-                    v_c_letras[1]=v_c_letras[1]+1  
-                elif j=='p':
-                    v_c_letras[2]=v_c_letras[2]+1  
-                elif j=='A':
-                    v_c_letras[3]=v_c_letras[3]+1  
-                elif j=='a':
-                    v_c_letras[4]=v_c_letras[4]+1  
-                elif j=='S':
-                    v_c_letras[5]=v_c_letras[5]+1  
-                elif j=='s':
-                    v_c_letras[6]=v_c_letras[6]+1  
-                    
-        # Total de c-letras
-        total_c_letras=sum(v_c_letras)
-
-        # atribuindo percentuais ao vetor
-        perc_c_letras = []
-        tx=100/total_c_letras
-        for i in v_c_letras:
-            a=i*tx
-            perc_c_letras.append(a)
-        
-        prv.extend(perc_c_letras)
-        
-        c_letras_pct_dict[corpus] = prv
-
-    c_letras_pct_df = pd.DataFrame(c_letras_pct_dict, index = ['u', 'P', 'p', 'A', 'a', 'S','s'])
-
-    ## PREPARA A SAÍDA ##
-
-    saida = {'df': c_letras_pct_df}
-
-    saida['x'] = pd.DataFrame({corpus:c_letras_pct_df.index.values.tolist() for corpus in c_letras_pct_dict.keys()})
-
-    saida['hover_data'] = {corpus: {'u': 'Movimento: Uníssono',
-                                    'P': 'Movimento: Passo ascendente',
-                                    'p': 'Movimento: Passo descendente',
-                                    'A': 'Movimento: Arpejo ascendente',
-                                    'a': 'Movimento: Arpejo descendente',
-                                    'S': 'Movimento: Salto ascendente',
-                                    's': 'Movimento: salto descendente'} for corpus in c_letras_pct_dict.keys()}
-    
-    saida['graficos_por_linha'] = 4
-    
-    saida['titulo_do_grafico'] = 'Porcentagem de ocorrência das c-letras em cada corpus selecionado'
-    
     return saida
 
 def carrega_r_letras():
@@ -91,7 +32,7 @@ def carrega_r_letras():
     with open('r_letras_corpora.txt', 'r') as file:
         for linha in file:
             r_letras.append(linha)
-            
+
     # normalização
     x=r_letras
     r_letras1=[]
@@ -116,7 +57,7 @@ def carrega_r_letras():
         for i in r_letras1_dict[corpus]:
             a=ord(i)-97
             r_vetor[a] = r_vetor[a]+1
-        
+
         # atribuindo percentagens
         perc=[]
         tot=sum(r_vetor)
@@ -125,7 +66,7 @@ def carrega_r_letras():
         for i in r_vetor:
             a=i*tx
             perc.append(a)
-        
+
         r_letras_pct_dict[corpus] = perc
 
     r_letras_pct_df = pd.DataFrame(r_letras_pct_dict, index = ['a', 'b', 'c', 'd', 'e',
@@ -140,7 +81,7 @@ def carrega_r_letras():
     ## PREPARA A SAÍDA ##
 
     saida['x'] = pd.DataFrame({corpus:r_letras_pct_df.index.values.tolist() for corpus in r_letras_pct_dict.keys()})
-    
+
     saida['hover_data'] = {corpus: {'a': 'Pontos de ataque: □□□ □□□ □□□ □□□',
                                     'b': 'Pontos de ataque: ■□□ □□□ □□□ □□□',
                                     'c': 'Pontos de ataque: □□□ ■□□ □□□ □□□',
@@ -167,9 +108,9 @@ def carrega_r_letras():
                                     'x': 'Pontos de ataque: ■□□ ■□□ ■□■ □■□',
                                     'y': 'Pontos de ataque: □□■ □■□ ■□■ □■□',
                                     'z': 'Pontos de ataque: ■□■ □■□ □■□ □■□'} for corpus in r_letras_pct_dict.keys()}
-    
+
     saida['graficos_por_linha'] = 2
-    
+
     saida['titulo_do_grafico'] = 'Porcentagem de ocorrência das r-letras em cada corpus selecionado'
 
     return saida
@@ -180,20 +121,20 @@ def carrega_genera_tas_acordes():
     with open('acordes_especificos_corpora.txt', 'r') as file:
         for linha in file:
             acordes.append(linha.replace('\n',''))
-    
+
     ## ABRE LÉXICO E ARMAZENA INFOS ##
     # abrindo o LÉXICO de TAs
-    LEX = pd.read_excel('LEXICO TAs - Python.xls', header = None) 
+    LEX = pd.read_excel('LEXICO TAs - Python.xls', header = None)
     LEX1=LEX.values.tolist()
 
     #separando no LÉXICO os tipos das cifras, para permitir a comparação (1) TAs
     tipos=LEX1[1]
 
     # separando no LÉXICO os tipos das cifras, para permitir a comparação (2) cifras
-    cifras=LEX1[2] 
+    cifras=LEX1[2]
 
     # *************** MARACUTAIA PARA DAR CERTO!
-    #artifício para eviatr "nan" na tríade maior (um vazio resultante da própria convenção adotada, que não emprega 
+    #artifício para eviatr "nan" na tríade maior (um vazio resultante da própria convenção adotada, que não emprega
     #símbolo para denotar uma tríade maior)
     cifras[104]=''
 
@@ -238,7 +179,7 @@ def carrega_genera_tas_acordes():
 
     for corpus in genera_dict.keys():
         # estabelecendo as condições de calcular as frequências dos genera no corpus
-        contador_g=dict(Counter(genera_dict[corpus])) 
+        contador_g=dict(Counter(genera_dict[corpus]))
         # frequências dos genera
         # contador_g_freq=contador_g.values() 
         # identificação dos genera
@@ -248,10 +189,10 @@ def carrega_genera_tas_acordes():
 
         #Vetor genus
         v_genus=10*[0]
-        
+
         #convertendo os valores ordenados em lista
         # g_corpus=list(contador_g_ordenados)
-        
+
         #preenchendo o vetor
         for i in contador_g_ordenados:
             a=i[0]
@@ -265,23 +206,23 @@ def carrega_genera_tas_acordes():
             elif a==87:
                 v_genus[3]=v_genus[3]+b
             elif a==86:
-                v_genus[4]=v_genus[4]+b        
+                v_genus[4]=v_genus[4]+b
             elif a==122:
                 v_genus[5]=v_genus[5]+b
             elif a==121:
-                v_genus[6]=v_genus[6]+b        
+                v_genus[6]=v_genus[6]+b
             elif a==120:
                 v_genus[7]=v_genus[7]+b
             elif a==119:
                 v_genus[8]=v_genus[8]+b
             elif a==118:
                 v_genus[9]=v_genus[9]+b
-                
+
         # Estabelecendo percentuais dos 10 genera
         perc=[]
         tot=sum(v_genus)
         tx=100/tot
-        
+
         for i in v_genus:
             perc.append(i*tx)
 
@@ -321,25 +262,25 @@ def carrega_genera_tas_acordes():
 
     for corpus in tas_dict.keys():
         # estabelecendo as condições de calcular as frequências dos TAs no corpus
-        contadorTA=dict(Counter(tas_dict[corpus])) 
+        contadorTA=dict(Counter(tas_dict[corpus]))
         # frequências dos TAs
         # contadorTA_01=contadorTA.values() 
         # identificando os TAs (genus + variantes)
         # contadorTA_02 = contadorTA.keys()
         # ordenando decrescentemente
-        TAs_corpus_ordenados=sorted(contadorTA.items(), key=lambda pair: pair[1], reverse=True) 
-        
+        TAs_corpus_ordenados=sorted(contadorTA.items(), key=lambda pair: pair[1], reverse=True)
+
         # desempacotando os TAs do corpus (ordenados)
         TAs_mais_comuns = []
         for item in TAs_corpus_ordenados:
             TAs_mais_comuns.append(item[0])
-            
+
         # TAs distintos
         # total_TAs_distintos=len(TAs_corpus_ordenados) 
-        
+
         # formando vetor número de TAs distintos, para armazenamento
         # TAs_dist = [total_TAs_distintos, total_tas_dict[corpus]]
-                
+
         # traduzindo TAs do corpus
         TAs_traduzidos=[]
         x=''
@@ -348,21 +289,21 @@ def carrega_genera_tas_acordes():
             b=str(i[1])
             c=a+b
             TAs_traduzidos.append(c)
-            
+
         # desempacotando as frequências (ordenados)
         frequencias = []
         for item in TAs_corpus_ordenados:
             frequencias.append(item[1])
-            
+
         # atribuindo percentuais 
         s=sum(frequencias)
         tx=100/s
-        
+
         perc_freq=[]
         for i in frequencias:
             a=i*tx
             perc_freq.append(a)
-        
+
         # limitando o número de TAs. a 40 para fins de plotagem (quando for o caso)
         if len(TAs_traduzidos)> 40:
             TAs_traduzidos_lim=TAs_traduzidos[0:40]
@@ -370,7 +311,7 @@ def carrega_genera_tas_acordes():
         else:
             TAs_traduzidos_lim=TAs_traduzidos
             frequencias_ordenadas_lim=perc_freq
-        
+
         tas_dict_pct[corpus] = frequencias_ordenadas_lim
         tas_dict_pct_x[corpus] = TAs_traduzidos_lim
 
@@ -400,7 +341,7 @@ def carrega_genera_tas_acordes():
 
     for corpus in ac_espec_dict.keys():
         # estabelecendo as condições de calcular as frequências dos acordes esp. no corpus
-        contador_ac=dict(Counter(ac_espec_dict[corpus])) 
+        contador_ac=dict(Counter(ac_espec_dict[corpus]))
         # frequências dos acordes esp.
         # contador_ac_freq=contador_ac.values() 
         #número de ac. esp. distintos no corpus
@@ -412,14 +353,14 @@ def carrega_genera_tas_acordes():
         # contador_ac_esp = contador_ac.keys() 
 
         # ordenando decrescentemente
-        AC_corpus_ordenados=sorted(contador_ac.items(), key=lambda pair: pair[1], reverse=True) 
+        AC_corpus_ordenados=sorted(contador_ac.items(), key=lambda pair: pair[1], reverse=True)
 
         # desempacotando os ac. esp. do corpus (ordenados)
         AC_mais_comuns = []
 
         for item in AC_corpus_ordenados:
             AC_mais_comuns.append(item[0])
-            
+
         # traduzindo Ac. esp. mais comuns (1a parte)
         AC_traduzidos=[]
         apenas_fundamentais=[]
@@ -432,25 +373,25 @@ def carrega_genera_tas_acordes():
             elif x==1:
                 y='C#'
             elif x==2:
-                y='D'   
+                y='D'
             elif x==3:
-                y='Eb'   
+                y='Eb'
             elif x==4:
                 y='E'
             elif x==5:
-                y='F'   
+                y='F'
             elif x==6:
-                y='F#'   
+                y='F#'
             elif x==7:
                 y='G'
             elif x==8:
-                y='Ab'   
+                y='Ab'
             elif x==9:
-                y='A'   
+                y='A'
             elif x==10:
                 y='Bb'
             elif x==11:
-                y='B'                    
+                y='B'
             a=chr(i[1])
             b=str(i[2])
             c=y+a+b
@@ -460,7 +401,7 @@ def carrega_genera_tas_acordes():
             apenas_TAs.append(d)
 
         # tradução dos acordes específicos do corpus em cifras alfanuméricas
-        ac_cifrados_mais_comuns=[]  
+        ac_cifrados_mais_comuns=[]
         conta=0
         for i in apenas_TAs:
             x=tipos.index(i)
@@ -469,25 +410,25 @@ def carrega_genera_tas_acordes():
             w=z+y
             ac_cifrados_mais_comuns.append(w)
             conta=conta+1
-            
+
         # desempacotando as frequências (ordenados)
         frequencias_ordenadas = []
         for item in AC_corpus_ordenados:
             frequencias_ordenadas.append(item[1])
-            
+
         # limitando o número de acordes esp. a 40 para fins de plotagem (quando for o caso)
         if len(ac_cifrados_mais_comuns)> 40:
             ac_cifrados_mais_comuns_lim=ac_cifrados_mais_comuns[0:40]
             frequencias_ordenadas_lim=frequencias_ordenadas[0:40]
-        else: 
+        else:
             ac_cifrados_mais_comuns_lim=ac_cifrados_mais_comuns
             frequencias_ordenadas_lim=frequencias_ordenadas
 
         # percentuais
-        s=sum(frequencias_ordenadas) 
+        s=sum(frequencias_ordenadas)
         tx=100/s
 
-        perc=[] 
+        perc=[]
         for i in frequencias_ordenadas_lim:
             a=i*tx
             perc.append(a)
